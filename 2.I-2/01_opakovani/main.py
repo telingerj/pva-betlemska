3e21152d264e57eaf5877f3a3b88ed5672b4e67d
Jan Telinger <jan.telinger@proton.me>
2026-09-03 16:19:05 +0200

    prepare class directories

---
+++ 2.I-2/01_opakovani/main.py
@@ -0,0 +1,34 @@
+# proměnné, datové typy
+# zeptat se uživatele na hodnotu a vypsat, jestli se jedná o celé číslo, desetinné číslo nebo řetězec
+# celé číslo vynásobit dvěma, desetinné číslo zaokrouhlit na celé a pro řetězec vypsat jeho délku
+
+
+# cykly
+# ptát se na číslo tak dlouho, dokud uživatel nezadá "konec"
+# vypsat součet všech těchto čísel
+
+# vypsat čísla od 1 do 100
+#  - běžně
+#  - pozpátku
+#  - pouze sudá čísla
+
+
+# seznamy (řetězce)
+# zeptat se na textový řetězec, vypsat
+#  - první znak
+#  - poslední znak
+#  - vypsat řetězec pozpátku
+
+
+# soubory
+# vytvořit soubor 0.txt, zapsat do něj vstup od uživatele
+# vytvořit soubor 1.txt, který bude obsahovat počet znaků v souboru 0.txt
+
+
+# OOP
+# třída člověk obsahující jméno, příjmení, datum narození
+# třída auto obsahující značku, rok výroby, barvu, nájezd, řidiče a majitele
+
+# majitel auta může přepsat auto na někoho jiného
+# majitel auta může autu přidat řidiče
+# řidič může s autem ujet nějakou vzdálenost, přičte se do nájezdu
