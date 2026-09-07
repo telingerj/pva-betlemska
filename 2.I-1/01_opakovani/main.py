edbb565465931ffe149c9d9d0b1fb107e9e8f96e
Jan Telinger <telingerj@skola.betlemska.cz>
2026-09-07 09:35:47 +0200

    add lesson 01

--- 2.I-1/01_opakovani/main.py
+++ 2.I-1/01_opakovani/main.py
@@ -1,23 +1,91 @@
 # proměnné, datové typy
 # zeptat se uživatele na hodnotu a vypsat, jestli se jedná o celé číslo, desetinné číslo nebo řetězec
 # celé číslo vynásobit dvěma, desetinné číslo zaokrouhlit na celé a pro řetězec vypsat jeho délku
+# nápověda: použijte try - except
 
+"""
+
+vstup = input("zadej vstup: ")
+try:
+    vstup = int(vstup)
+    print("dvojnásobek čísla:", vstup * 2)
+except:
+    try:
+        vstup = float(vstup)
+        print("zaokrouhlený vstup:", round(vstup))
+    except:
+        print(len(vstup))
+
+"""
 
 # cykly
 # ptát se na číslo tak dlouho, dokud uživatel nezadá "konec"
 # vypsat součet všech těchto čísel
+
+"""
+soucet = 0
+while True:
+    vstup = input("zadej číslo: ")
+    if vstup == "konec":
+        break
+    vstup = int(vstup)
+    soucet += vstup
+print("součet čísel je:", soucet)
+"""
+
 
 # vypsat čísla od 1 do 100
 #  - běžně
 #  - pozpátku
 #  - pouze sudá čísla
 
+"""
+
+for i in range(1, 101):
+    print(i, end=" ")
+print()
+
+for i in range(100, 0, -1):
+    print(i, end=" ")
+print()
+
+for i in range(2, 101, 2):
+    print(i, end=" ")
+print()
+
+"""
 
 # seznamy (řetězce)
 # zeptat se na textový řetězec, vypsat
 #  - první znak
 #  - poslední znak
 #  - vypsat řetězec pozpátku
+#  - zjistit, jestli je text palindrom (text, který se píše stejně pozpátku jako běžně) (kobylamamalybok)
+
+vstup = input("zadej vstup: ")
+
+print("první znak:", vstup[0])
+print("poslední znak:", vstup[len(vstup) - 1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # soubory
